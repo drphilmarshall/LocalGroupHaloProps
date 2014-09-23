4520d6226d65780be1dafd344b455ecd561134b1
Marc Williamson <marxwillia@gmail.com>
2014-09-23 16:15:50 -0700

    started likelihood class

---
+++ localgroup/likelihood.py
@@ -0,0 +1,55 @@
+# ======================================================================
+
+import localgroup
+
+import numpy
+
+# ======================================================================
+
+class ApproximateLikelihood(object):
+    """
+    NAME
+        Likelihood
+
+    PURPOSE
+        Compute the likelihood of a given halo having the observed properties.
+
+    COMMENTS
+        This class allows you to generate the approximation used, as well as evalu        ate the likelihood. 
+
+    INITIALISATION
+
+    
+    METHODS
+        generate         - draw sample vector from observations' distribuions
+        approximate      - compute KNN/GMM/etc estimate of PDF
+        evaluate         - compute value of PDF at given vector
+        
+        NB. "vector" refes to a position in 6D MW-M31 D,vr,vt space
+
+    BUGS
+
+    AUTHORS
+      This file is part of the LocalGroupHaloProps project, 
+      distributed under the GPL v2, 
+      by Marc Williamson Phil Marshall (KIPAC). 
+      Please cite: Williamson et al in preparation.
+
+    HISTORY
+      2014-09-23  started Williamson and Marshall (KIPAC)
+    """
+# ======================================================================
+
+    def __init__(self):
+    
+        return
+        
+# ----------------------------------------------------------------------------
+
+    def generate(self,mode="observational"):
+
+
+                
+        return
+        
+# ----------------------------------------------------------------------------
