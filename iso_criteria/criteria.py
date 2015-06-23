eb445f6c4cd38c0cac0a70d33f523c22f0ebd909
Marc E. Williamson <mwillia1@rhel6-64c.slac.stanford.edu>
2015-06-23 10:20:58 -0700

    Modified iso criteria to find quads, no longer supports _sub_larger type labeling.

--- iso_criteria/criteria.py
+++ iso_criteria/criteria.py
@@ -34,13 +34,11 @@ def find_largest_sub(tree, center, D, halos, pair_idx, vmax_cut=None, periodic=T
 
 def isolated(pairs, halos, host_flag, D_iso, D_M33, box_size, vmax_cut=None, periodic=True):
     print 'before: ', len(pairs)
-#    iso_pairs = []
-    MW_M31_larger = []
-    M31_M31_larger = []
-    M33_M31_larger = []
-    M33_M33_larger = []
-    M31_M33_larger = []
-    MW_M33_larger = []
+    larger_host = []
+    larger_host_sub = []
+    smaller_host = []
+    smaller_host_sub = []
+
     with fast3tree(halos[list('xyz')].view(float).reshape(-1, 3)) as tree:
         for pair in pairs:
             pair_idx = host_flag[list(pair)]
@@ -58,47 +56,46 @@ def isolated(pairs, halos, host_flag, D_iso, D_M33, box_size, vmax_cut=None, per
 
             # the second index has a smaller mass
             if h2['mvir'] == halos['mvir'][idx[biggest]]:
-#                iso_pairs.append(pair) #satisfying iso
-                M31_M31_larger.append(pair[0])
-                MW_M31_larger.append(pair[1])
+                larger_host.append(pair[0])
+                smaller_host.append(pair[1])
             else:
                 continue #no need to search M33
 
             #find M33
             M33_ind, M33_mass = find_largest_sub(tree, p1, D_M33, halos, pair_idx, vmax_cut, periodic)
             LMC_ind, LMC_mass = find_largest_sub(tree, p2, D_M33, halos, pair_idx, vmax_cut, periodic)
-            M33_M31_larger.append(M33_ind)#assumes M31 is p1 (ie larger than MW)
-            if LMC_mass > M33_mass:
-                M33_ind = LMC_ind
-                M31_M33_larger.append(pair[1]) #append the smaller mass index in the pair -> M31 since LMC > M33
-                MW_M33_larger.append(pair[0]) #append the larger mass index in the pair -> MW since LMC > M33
-            else:
-                M31_M33_larger.append(pair[0]) #append the smaller mass index in the pair -> M31 since LMC < M33
-                MW_M33_larger.append(pair[1]) #append the larger mass index in the pair -> MW since LMC < M33
-            M33_M33_larger.append(M33_ind)
+            larger_host_sub.append(M33_ind)
+            smaller_host_sub.append(LMC_ind)
 
-    print 'after: ', len(MW_M31_larger)
-    return MW_M31_larger, M31_M31_larger, M33_M31_larger, M33_M33_larger, M31_M33_larger, MW_M33_larger
+    print 'after: ', len(larger_host)
+    return larger_host, larger_host_sub, smaller_host, smaller_host_sub
 
 
-def get_data(halos, host_flag, MW_inds, M31_inds, M33_inds):
+def get_data(halos, host_flag, MW_inds, M31_inds, M33_inds, LMC_inds):
     fn = ['MW_'+nm for nm in halos.dtype.names]
     fn1 = ['M31_'+nm for nm in halos.dtype.names]
     fn2 = ['M33_'+nm for nm in halos.dtype.names]
-    fields = np.hstack((np.array(fn), np.array(fn1), np.array(fn2)))
+    fn3 = ['LMC_' +nm for nm in halos.dtype.names]
+    fields = np.hstack((np.array(fn), np.array(fn1), np.array(fn2), np.array(fn3)))
     dtype = np.dtype([(f, int if f.endswith('id') else float) for f in fields])
     num_sys = len(MW_inds)
     data = np.empty(num_sys, dtype)
-    no_M33_data = np.copy(halos[0])
-    for f in no_M33_data.dtype.names: no_M33_data[f]=-1
+    no_sub_data = np.copy(halos[0])
+    for f in no_sub_data.dtype.names: no_sub_data[f]=-1
     mw_dat = halos[host_flag[MW_inds]]
     m31_dat = halos[host_flag[M31_inds]]
-    m33_dat = np.array([halos[i] if i>0 else no_M33_data for i in M33_inds])
+    m33_dat = np.array([halos[i] if i>0 else no_sub_data for i in M33_inds])
+    lmc_dat = np.array([halos[i] if i>0 else no_sub_data for i in LMC_inds])
     for i in range(len(data)):
-        data[i] = tuple(mw_dat[i])+tuple(m31_dat[i])+tuple(m33_dat[i])
+        data[i] = tuple(mw_dat[i])+tuple(m31_dat[i])+tuple(m33_dat[i])+tuple(lmc_dat[i])
     return data
 
 def get_trip_data(data):
     trip_data = data[data['M33_id']!=-1]
     return trip_data
+
+def get_quad_data(data):
+    quad_data = data[data['M33_id']!=-1]
+    quad_data = quad_data[quad_data['LMC_id']!=-1]
+    return quad_data
 
