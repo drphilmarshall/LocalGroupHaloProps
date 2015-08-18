305cfa7e86cbebab90ad9592b5d4cf250d533d8f
Marc E. Williamson <mwillia1@rhel6-64f.slac.stanford.edu>
2015-08-18 11:53:13 -0700

    Timing Arg Plot made

---
+++ final_scripts/gmm_pair_exist.py
@@ -0,0 +1,96 @@
+import matplotlib
+matplotlib.use('Agg')
+from matplotlib.pyplot import *
+import localgroup
+import triangle
+import sklearn
+from sklearn import mixture
+import numpy as np
+import pickle
+import matplotlib.patches as mpatches
+
+save_path = '/afs/slac.stanford.edu/u/ki/mwillia1/Thesis/LocalGroupHaloProps/working_plots/pair_exist/02/'
+
+L = localgroup.Likelihood(isPair=True)
+L.generate(Nsamples=200000)
+
+L.set_PDF(mixture.GMM(n_components=10, covariance_type='full'))
+L.approximate()
+
+figure_obs = L.plot_samples(10, color='b', overlay=False)
+
+figure_model = L.model_gof(L.T.Nsamples, color="r", fig=None)
+
+L.model_gof(L.T.Nsamples, color="r", fig=figure_obs)
+red_patch = mpatches.Patch(color='red')
+blue_patch = mpatches.Patch(color='blue')
+figure_obs.legend(handles=[red_patch, blue_patch], labels=["Model Generated", "Observation Generated"])
+
+path = '/afs/slac.stanford.edu/u/ki/mwillia1/All_Boxes_quad_dat_M31_larger.npy'
+npoints = 200000
+halo_props = ['MW_Mvir', 'M31_Mvir', 'M33_Mvir']
+
+Tr = localgroup.Triplet(isPair=False)
+Tr.read_sim_points(path, npoints, halo_props, h=0.7, a=1.0, npy=True)
+
+Tr.transform_to_M31(sim=True)
+
+Tr.dist_filter((Tr.sim_samples[:,0] < 1) & (Tr.sim_samples[:,3] < 0.4) & (Tr.sim_samples[:,6] < 1))
+
+Tr.sim_samples = Tr.sim_samples[:,0:3]
+
+Tr.preprocess(L.samples_means, L.samples_stds, mode='sim')
+
+dat = np.transpose(np.vstack((np.transpose(Tr.sim_samples), np.log10(Tr.M31.Mvir), np.log10(Tr.MW.Mvir), np.log10(Tr.M33.Mvir), np.log10(Tr.LMC.Mvir))))
+Tr.GMM(20, dat)
+
+Tr.GMM_sample(10000000, L, reps=1, simple=False)
+path = '/afs/slac.stanford.edu/u/ki/mwillia1/Thesis/LocalGroupHaloProps/Tr_PE_samp.pickle'
+with open(save_path, 'wb') as f:
+    pickle.dump(Tr.gmm_samples, f)
+
+"""
+gmm_MW = np.copy(Tr.gmm_samples[:,4])
+gmm_M31 = np.copy(Tr.gmm_samples[:,3])
+gmm_LG = np.log10(np.power(10,gmm_MW) + np.power(10,gmm_M31))
+gmm_LMC = np.copy(Tr.gmm_samples[:,6])
+gmm_M33 = np.copy(Tr.gmm_samples[:,5])
+
+Tr.gmm_samples = Tr.gmm_samples[:,0:3]
+
+Tr.compute_model_weights(L, 'gmm')
+
+count, smallest_weight = Tr.calculate_N95(filter_samples=False)
+print "N95 ", count
+
+Tr.unprocess(L.samples_means, L.samples_stds, 'gmm')
+data2 = np.transpose(np.vstack((np.transpose(Tr.gmm_samples), gmm_MW, gmm_M31, gmm_M33, gmm_LMC)))
+labs = ["$D^{\\rm M31} Mpc$", "$v_{\\rm rad}^{\\rm M31} km/s$", "$v_{\\rm tan}^{\\rm M31} km/s$","$Mvir_{MW}$", "$Mvir_{M31}$", "$Mvir_{M33}$", "$Mvir_{LMC}$"]
+pl = triangle.corner(data2, labels=labs, quantiles=[0.16,0.5,0.84], fig=None, weights=None,\
+                         plot_contours=True, show_titles=True, title_args={"fontsize": 16}, label_args={"fontsize": 16},\
+                         plot_datapoints=False, bins=50, color='b')
+Tr.preprocess(L.samples_means, L.samples_stds, mode='gmm')
+
+Tr.unprocess(L.samples_means, L.samples_stds, mode='sim')
+data = np.transpose(np.vstack((np.transpose(Tr.sim_samples), np.log10(Tr.MW.Mvir), np.log10(Tr.M31.Mvir), np.log10(Tr.M33.Mvir), np.log10(Tr.LMC.Mvir))))
+labs = ["$D^{\\rm M31} Mpc$", "$v_{\\rm rad}^{\\rm M31} km/s$", "$v_{\\rm tan}^{\\rm M31} km/s$","$Mvir_{MW}$", "$Mvir_{M31}$", "$Mvir_{M33}$", "$Mvir_{LMC}$"]
+sim_plot = triangle.corner(data, labels=labs, quantiles=[0.16,0.5,0.84], fig=pl, weights=None,\
+                         plot_contours=True, show_titles=False, title_args={"fontsize": 12}, \
+                         plot_datapoints=False, bins=50, color='r', label_kwargs={"fontsize": 16}, label_args={"fontsize": 16})
+red_patch = mpatches.Patch(color='r')
+cyan_patch = mpatches.Patch(color='b')
+sim_plot.legend(handles=[red_patch, cyan_patch], labels=["CONSUELO Prior", "GMM-fit CONSUELO Prior"], fontsize=16)
+sim_plot.suptitle("GMM Prior Overlayed with Consuelo Prior", fontsize=16)
+Tr.preprocess(L.samples_means, L.samples_stds, mode='sim')
+
+sim_plot.savefig(save_path+'P_GMMP_GOF.pdf', dpi=600)
+
+
+
+
+labs = ["$Mvir_{MW}$", "$Mvir_{M31}$", "$Mvir_{M33}$", "$Mvir_{LMC}$", "$Mvir_{LG}$"]
+all_mvir = np.transpose(np.vstack((gmm_MW, gmm_M31, gmm_M33, gmm_LMC, gmm_LG)))
+figure = triangle.corner(all_mvir, labels=labs, quantiles=[0.16,0.5,0.84], fig=None, weights=Tr.weights,                         plot_contours=True, show_titles=True, title_args={"fontsize": 16}, label_args={"fontsize": 16},                         plot_datapoints=False, bins=20, color='c')
+#figure.suptitle("Weighted Mass Posterior PDF, GMM Prior", fontsize=16, horizontalalignment='left')
+figure.savefig(save_path+'P_GMMP_all_Mvir.pdf', dpi=800)
+"""
--- final_scripts/gmm_prior_pair.py
+++ final_scripts/gmm_prior_pair.py
@@ -13,11 +13,13 @@ import sys
 
 exist = int(sys.argv[1])
 if exist:
-    save_path = '/afs/slac.stanford.edu/u/ki/mwillia1/Thesis/LocalGroupHaloProps/working_plots/pair_exist/'
-    path = '/lustre/ki/pfs/mwillia1/LG_project/Consuelo_Boxes/All_Boxes_quad_dat_M31_larger.npy' 
+    save_path = '/afs/slac.stanford.edu/u/ki/mwillia1/Thesis/LocalGroupHaloProps/working_plots/pair_exist/02/'
+#    path = '/lustre/ki/pfs/mwillia1/LG_project/Consuelo_Boxes/All_Boxes_quad_dat_M31_larger.npy' 
+    path = '/afs/slac.stanford.edu/u/ki/mwillia1/All_Boxes_dat_M31_larger.npy'
 else:
-    save_path = '/afs/slac.stanford.edu/u/ki/mwillia1/Thesis/LocalGroupHaloProps/working_plots/pair/'
-    path = '/lustre/ki/pfs/mwillia1/LG_project/Consuelo_Boxes/All_Boxes/Pairs/All_Boxes_dat_M31_larger.npy'
+    save_path = '/afs/slac.stanford.edu/u/ki/mwillia1/Thesis/LocalGroupHaloProps/working_plots/pair/02/'
+    path = '/afs/slac.stanford.edu/u/ki/mwillia1/All_Boxes_dat_M31_larger.npy'
+#    path = '/lustre/ki/pfs/mwillia1/LG_project/Consuelo_Boxes/All_Boxes/Pairs/All_Boxes_dat_M31_larger.npy'
 # Inside the Likelihood object is a "triplet" object called T, which contains an array of sample local groups, each with kinematic parameters consistent with the observational data. Let's plot these kinematic parameters in a "triangle" figure, to show all their 1 and 2-D marginal distributions.
 
 # In[2]:
@@ -83,8 +85,11 @@ halo_props = ['MW_Mvir', 'M31_Mvir', 'M33_Mvir']
 
 
 # In[10]:
+if exist:
+    Tr = localgroup.Triplet(isPair=False)
+else:
+    Tr = localgroup.Triplet(isPair=True)
 
-Tr = localgroup.Triplet(isPair=True)
 Tr.read_sim_points(path, npoints, halo_props, h=0.7, a=1.0, npy=True)
 
 #Tr_cut = localgroup.Triplet(isPair=False)
@@ -105,10 +110,12 @@ Tr.transform_to_M31(sim=True)
 
 # In[13]:
 
-Tr.dist_filter((Tr.sim_samples[:,0] < 1))
+if exist:
+    Tr.dist_filter((Tr.sim_samples[:,0] < 1) & (Tr.sim_samples[:,3] < 0.4) & (Tr.sim_samples[:,6] < 1))
+else:
+    Tr.dist_filter((Tr.sim_samples[:,0] < 1))
 
-#Tr_cut.dist_filter((Tr_cut.sim_samples[:,0] < 1) & (Tr_cut.sim_samples[:,3] < 0.4))
-
+if exist: Tr.sim_samples = Tr.sim_samples[:,0:3]
 
 # In[14]:
 
@@ -126,18 +133,26 @@ sim_plot = Tr.plot_kinematics('sim', L.samples_means, L.samples_stds, color='c',
 
 #sim_plot = Tr.plot_kinematics('sim', L.samples_means, L.samples_stds, color='c', fig=None)
 # In[ ]:
+if exist:
+    dat = np.transpose(np.vstack((np.transpose(Tr.sim_samples), np.log10(Tr.M31.Mvir), np.log10(Tr.MW.Mvir), np.log10(Tr.M33.Mvir), np.log10(Tr.LMC.Mvir))))
+else:
+    dat = np.transpose(np.vstack((np.transpose(Tr.sim_samples), np.log10(Tr.M31.Mvir), np.log10(Tr.MW.Mvir))))
 
-dat = np.transpose(np.vstack((np.transpose(Tr.sim_samples), np.log10(Tr.M31.Mvir), np.log10(Tr.MW.Mvir), Tr.M31.Cvir, Tr.MW.Cvir)))
-#dat = np.transpose(np.vstack((np.transpose(Tr.sim_samples), np.log10(Tr.M31.Mvir), np.log10(Tr.MW.Mvir))))
-Tr.GMM(20, dat)
+Tr.GMM(5, dat)
 
+if exist:
+    Tr_save_path = "/afs/slac.stanford.edu/u/ki/mwillia1/Thesis/LocalGroupHaloProps/Tr_exist.pickle"
+    Tr.save(Tr_save_path) 
+else:
+    Tr_save_path = "/afs/slac.stanford.edu/u/ki/mwillia1/Thesis/LocalGroupHaloProps/Tr_pair.pickle"
+    Tr.save(Tr_save_path)
 #dat = np.transpose(np.vstack((np.transpose(Tr_cut.sim_samples), np.log10(Tr_cut.M31.Mvir), np.log10(Tr_cut.MW.Mvir), np.log10(Tr_cut.M33.Mvir))))
 #Tr_cut.GMM(20, dat)
 
 
 # In[ ]:
 
-Tr.GMM_sample(100000000)
+Tr.GMM_sample(500000, L, reps=1, simple=True)
 
 #Tr_cut.GMM_sample(20000000)
 
@@ -147,25 +162,16 @@ Tr.GMM_sample(100000000)
 
 gmm_MW = np.copy(Tr.gmm_samples[:,4])
 gmm_M31 = np.copy(Tr.gmm_samples[:,3])
-
-gmm_MW_C = np.copy(Tr.gmm_samples[:,6])
-gmm_M31_C = np.copy(Tr.gmm_samples[:,5])
-#gmm_M33_C = np.copy(Tr.gmm_samples[:,11])
-
-#gmm_LG = np.log10(np.power(10,gmm_MW) + np.power(10,gmm_M31))
-
-#cond = gmm_MW < gmm_M31
-#Tr.gmm_samples = Tr.gmm_samples[cond]
-#gmm_MW = gmm_MW[cond]
-#gmm_M31 = gmm_M31[cond]
-#gmm_M33 = gmm_M33[cond]
-#gmm_LG = gmm_LG[cond]
+gmm_LG = np.log10(np.power(10,gmm_MW) + np.power(10,gmm_M31)) 
+if exist:
+    gmm_LMC = np.copy(Tr.gmm_samples[:,6])
+    gmm_M33 = np.copy(Tr.gmm_samples[:,5])
 
 Tr.gmm_samples = Tr.gmm_samples[:,0:3]
 
 
 
-Tr.compute_model_weights(L, 'gmm', normalize=True, split=5)
+Tr.compute_model_weights(L, 'gmm', normalize=True, split=1)
 
 #Tr_cut.compute_model_weights(L, 'gmm')
 
@@ -174,35 +180,41 @@ Tr.compute_model_weights(L, 'gmm', normalize=True, split=5)
 
 count, smallest_weight = Tr.calculate_N95(filter_samples=False)
 print "N95 ", count
-print "smallest weight ", smallest_weight
-cond = Tr.weights[:] > smallest_weight
-gmm_MW = gmm_MW[cond]
-gmm_M31 = gmm_M31[cond]
-
-gmm_MW_C = gmm_MW_C[cond]
-gmm_M31_C = gmm_M31_C[cond]
-Tr.gmm_samples = Tr.gmm_samples[cond]
-Tr.weights = Tr.weights[cond]
-#print Tr_cut.calculate_N95()
-
-
-# In[18]:
+#print "smallest weight ", smallest_weight
+#cond = Tr.weights[:] > smallest_weight
+#gmm_MW = gmm_MW[cond]
+#gmm_M31 = gmm_M31[cond]
+#gmm_LG = gmm_LG[cond]
+#if exist:
+#    gmm_LMC = gmm_LMC[cond]
+#    gmm_M33 = gmm_M33[cond]
+#
+#Tr.gmm_samples = Tr.gmm_samples[cond]
+#Tr.weights = Tr.weights[cond]
 
 
 Tr.unprocess(L.samples_means, L.samples_stds, 'gmm')
 #data2 = np.transpose(np.vstack((np.transpose(Tr.gmm_samples), gmm_MW, gmm_M31, gmm_M33, gmm_MW_C, gmm_M31_C, gmm_M33_C)))
-data2 = np.transpose(np.vstack((np.transpose(Tr.gmm_samples), gmm_MW, gmm_M31, gmm_MW_C, gmm_M31_C)))
-#labs=["mwd", "mwvr", "mwvt", "m33d", "m33vr", "m33vt", "MWMvir", "M31Mvir", "M33Mvir"]
-#labs=["mwd", "mwvr", "mwvt", "m33d", "m33vr", "m33vt", "LMCd", "LMCvr", "LMCvt", "MWMvir", "M31Mvir"]
-labs = ["$D^{\\rm M31} Mpc$", "$v_{\\rm rad}^{\\rm M31} km/s$", "$v_{\\rm tan}^{\\rm M31} km/s$","$Mvir_{MW}$", "$Mvir_{M31}$", "$Cvir_{MW}$", "$Cvir_{M31}$"]
-#labs=["mwd", "mwvr", "mwvt", "m33d", "m33vr", "m33vt", "MWMvir", "M31Mvir", "M33Mvir", "MW Cvir", "M31 Cvir", "M33 Cvir"]
+if exist:
+    data2 = np.transpose(np.vstack((np.transpose(Tr.gmm_samples), gmm_MW, gmm_M31, gmm_M33, gmm_LMC)))
+    labs = ["$D^{\\rm M31} Mpc$", "$v_{\\rm rad}^{\\rm M31} km/s$", "$v_{\\rm tan}^{\\rm M31} km/s$","$Mvir_{MW}$", "$Mvir_{M31}$", "$Mvir_{M33}$", "$Mvir_{LMC}$"]
+else:
+    data2 = np.transpose(np.vstack((np.transpose(Tr.gmm_samples), gmm_MW, gmm_M31)))
+    labs = ["$D^{\\rm M31} Mpc$", "$v_{\\rm rad}^{\\rm M31} km/s$", "$v_{\\rm tan}^{\\rm M31} km/s$","$Mvir_{MW}$", "$Mvir_{M31}$"]
+
 pl = triangle.corner(data2, labels=labs, quantiles=[0.16,0.5,0.84], fig=None, weights=None,                         plot_contours=True, show_titles=True, title_args={"fontsize": 16}, label_args={"fontsize": 16},                          plot_datapoints=False, bins=20, color='b')
 Tr.preprocess(L.samples_means, L.samples_stds, mode='gmm')
 # In[ ]:
 
+if exist:
+    data = np.transpose(np.vstack((np.transpose(Tr.sim_samples), np.log10(Tr.MW.Mvir), np.log10(Tr.M31.Mvir), np.log10(Tr.M33.Mvir), np.log10(Tr.LMC.Mvir))))
+    labs = ["$D^{\\rm M31} Mpc$", "$v_{\\rm rad}^{\\rm M31} km/s$", "$v_{\\rm tan}^{\\rm M31} km/s$","$Mvir_{MW}$", "$Mvir_{M31}$", "$Mvir_{M33}$", "$Mvir_{LMC}$"]
+else:
+    data = np.transpose(np.vstack((np.transpose(Tr.sim_samples), np.log10(Tr.MW.Mvir), np.log10(Tr.M31.Mvir))))
+    labs = ["$D^{\\rm M31} Mpc$", "$v_{\\rm rad}^{\\rm M31} km/s$", "$v_{\\rm tan}^{\\rm M31} km/s$","$Mvir_{MW}$", "$Mvir_{M31}$"]
+
+
 Tr.unprocess(L.samples_means, L.samples_stds, mode='sim')
-data = np.transpose(np.vstack((np.transpose(Tr.sim_samples), np.log10(Tr.MW.Mvir), np.log10(Tr.M31.Mvir), Tr.MW.Cvir, Tr.M31.Cvir)))
-labs = ["$D^{\\rm M31} Mpc$", "$v_{\\rm rad}^{\\rm M31} km/s$", "$v_{\\rm tan}^{\\rm M31} km/s$","$Mvir_{MW}$", "$Mvir_{M31}$", "$Cvir_{MW}$", "$Cvir_{M31}$"]
 sim_plot = triangle.corner(data, labels=labs, quantiles=[0.16,0.5,0.84], fig=pl, weights=None,                         plot_contours=True, show_titles=True, title_args={"fontsize": 12},                          plot_datapoints=False, bins=20, color='r', label_kwargs={"fontsize": 16})
 red_patch = mpatches.Patch(color='r')
 cyan_patch = mpatches.Patch(color='b')
@@ -217,9 +229,18 @@ Tr.preprocess(L.samples_means, L.samples_stds, mode='sim')
 # In[ ]:
 
 #name = 'gmm_CONSUELO_prior.png'
-sim_plot.savefig(save_path+'Q_GMMP_GOF.png', dpi=600)
-sim_plot.savefig(save_path+'Q_GMMP_GOF.pdf', dpi=600)
+sim_plot.savefig(save_path+'P_GMMP_GOF.pdf', dpi=600)
+"""
+if exist:
+    labs = ["$Mvir_{MW}$", "$Mvir_{M31}$", "$Mvir_{M33}$", "$Mvir_{LMC}$", "$Mvir_{LG}$"]
+    all_mvir = np.transpose(np.vstack((gmm_MW, gmm_M31, gmm_M33, gmm_LMC, gmm_LG)))
+else:
+    labs = ["$Mvir_{MW}$", "$Mvir_{M31}$", "$Mvir_{LG}$"]
+    all_mvir = np.transpose(np.vstack((gmm_MW, gmm_M31, gmm_LG)))
 
+figure = triangle.corner(all_mvir, labels=labs, quantiles=[0.16,0.5,0.84], fig=None, weights=Tr.weights,                         plot_contours=True, show_titles=True, title_args={"fontsize": 16}, label_args={"fontsize": 16},                         plot_datapoints=False, bins=20, color='c')
+#figure.suptitle("Weighted Mass Posterior PDF, GMM Prior", fontsize=16, horizontalalignment='left')
+figure.savefig(save_path+'P_GMMP_all_Mvir.pdf', dpi=800)
 
 
 # In[ ]:
@@ -239,4 +260,4 @@ figure = triangle.corner(all_mvir, labels=labs, quantiles=[0.16,0.5,0.84], fig=N
 figure.savefig(save_path+'Q_GMMP_M31_MvsC.png', dpi=800)
 figure.savefig(save_path+'Q_GMMP_M31_MvsC.pdf', dpi=800)
 # In[31]:
-
+"""
